e2ae723304cfed93aa2622807ac37947872419f6
Memo-MorontaFernandez <g.morontafernandez@gmail.com>
2024-03-03 18:50:12 -0700

    Resolving merge conflicts

--- app/Http/Controllers/ViewFormController.php
+++ app/Http/Controllers/ViewFormController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ViewClientForm;
+use App\Models\ClientForm;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ViewFormController extends Controller
 {
     //
     function index()
     {
-        $clientForm = ViewClientForm::all();
+        $clientId = 1;
+        $clientForm = ClientForm::where('FormID', 1)->first();
         return view('viewform.viewclientform', compact('clientForm'));
     }
 }
