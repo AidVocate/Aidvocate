1e84390e1c273e2f59b1ea1d037705c4f9c81cbc
Allaine Paredes <aparedes2@nait.ca>
2024-03-04 01:50:23 -0700

    complete listing LN form

--- app/Http/Controllers/ListFormController.php
+++ app/Http/Controllers/ListFormController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ListForm;
 
 class ListFormController extends Controller
 {
     //
     function ListFormView()
     {
-        return view('ListForm.ListLegalNeed');
+        $legalneed = ListForm::all();
+
+        return view('ListForm.ListLegalNeed', ['legalneed' => $legalneed]);
     }
 }
