35177578cec39c33a19d76032b694407ebd5119e
Forfiate <90063181+TheDarioAu@users.noreply.github.com>
2024-03-03 22:36:10 -0700

    Updated React

--- react/src/layouts/DefaultLayout.tsx
+++ react/src/layouts/DefaultLayout.tsx
@@ -2,8 +2,19 @@ import { Outlet }  from "react-router-dom";
 import Navigation from '../components/Navigation';
 
 export default function Layout() {
+  const backgroundImageUrl = 'url("/background.webp")';
   return (
-    <div>
+    <div
+        style={{
+          backgroundImage: backgroundImageUrl,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundAttachment: 'fixed',
+          minHeight: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >        
         <Navigation/>
         <Outlet/>
     </div>
--- react/src/main.tsx
+++ react/src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import router from './router.tsx'
 import { RouterProvider } from 'react-router-dom'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { StyledEngineProvider, ThemeProvider, createTheme } from '@mui/material/styles';
 import { ContextProvider } from './contexts/ContextProvider.tsx';
+import './index.css'
+
 const theme = createTheme({
   palette: {
       primary: {
@@ -17,10 +19,12 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <ContextProvider>
-        <RouterProvider router={router}/>
-      </ContextProvider>
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <ContextProvider>
+          <RouterProvider router={router}/>
+        </ContextProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>,
 )
