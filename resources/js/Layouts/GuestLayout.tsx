3993d6364cb0d19fbfb292cb3033eb4903bc25e9
Forfiate <90063181+TheDarioAu@users.noreply.github.com>
2024-04-08 01:34:50 -0600

    Update GuestLayout.tsx

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -3,8 +3,11 @@ import NavBar from '@/Components/NavBar';
 export default function Guest({ children }: Children) {
     return (
         <>
-            <NavBar/>
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <NavBar routes={[
+                {name: "Login", route: "login"},
+                {name: "Register", route: "register"}
+            ]}/>
+            <div className="w-full mx-auto sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {children}
             </div>
         </>
