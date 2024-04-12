71c4d57b4d5163bce71ed979c06f4c70c17420b8
Dario Au <90063181+TheDarioAu@users.noreply.github.com>
2024-04-12 01:46:29 -0600

    Role Specific Dashboard
    The dashboard page gets the Role/Index page and renders on the dashboard page

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,6 +1,10 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-
+import AdminDashboard from './Admin/Index';
+import ClientDashboard from './Client/Index';
+import LawyerDashboard from './Lawyer/Index';
+import PBODashboard from './PBO/Index';
+import Container from '@mui/material/Container';
 
 export default function Dashboard({ auth }: inertia.Auth) {
     return (
@@ -9,11 +13,27 @@ export default function Dashboard({ auth }: inertia.Auth) {
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
         >
             <Head title="Dashboard" />
-            <div>
+            <Container>
+                <RoleDashboard role={auth.user.Role}/>
                 <div className="mx-auto max-w-screen-lg my-20 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div className="p-6 text-gray-900">You're logged in!</div>
                 </div>
-            </div>
+            </Container>
         </AuthenticatedLayout>
     );
+}
+
+function RoleDashboard({role = ""}) {
+    switch (role) {
+        case "Admin":
+            return (<AdminDashboard/>)
+        case "Client":    
+            return (<ClientDashboard/>)
+        case "Lawyer":   
+            return (<LawyerDashboard/>)
+        case "PBO":
+            return (<PBODashboard/>)
+        default:
+            return (<></>)
+    }
 }
--- resources/js/types/inertia.d.ts
+++ resources/js/types/inertia.d.ts
@@ -7,6 +7,7 @@ declare global {
             email: string
             created_at: Date
             updated_at: Date
+            Role: string
         }
 
         export interface Jetstream {
