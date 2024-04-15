dd97eab7d754990832f002c75a4e3707f474dade
Dario Au <90063181+TheDarioAu@users.noreply.github.com>
2024-04-14 21:46:45 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,7 @@ Route::middleware(['auth', RoleMiddleware::class . ':Client'])->group(function (
 Route::middleware(['auth', RoleMiddleware::class . ':Lawyer'])->group(function () {
     Route::get('/lawyer', [LawyerController::class, 'index']);
     Route::get('/lawyer/ViewLegalNeed/{CaseID}', [LawyerController::class, 'ViewLegalNeed']);
-    Route::get('/lawyer/ViewLegalNeedBoard', [LawyerController::class, 'CaseList'])->name('cases.index');
+    Route::get('/lawyer/ViewLegalNeedBoard', [LawyerController::class, 'CaseList'])->name('lawyer.board');
     // Route::get('/lawyer/SubmitsOffer', [LawyerController::class, 'submitsOffer']);
     Route::post('/lawyer/SubmitsOffer/{CaseID}', [LawyerController::class, 'grabCase'])->name('cases.grab');
     Route::get('/lawyer/cases', [LawyerController::class, 'LawyerCaseList'])->name('lawyer.cases');
